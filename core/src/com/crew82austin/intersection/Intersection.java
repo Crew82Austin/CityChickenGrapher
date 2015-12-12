99d1299c364fbc0ea7cc65df74e79a54d50c5624
jrspotts <justinrspotts@gmail.com>
2015-12-12 15:28:08 -0600

    Added Intersection package and class

---
+++ core/src/com/crew82austin/intersection/Intersection.java
@@ -0,0 +1,57 @@
+package com.crew82austin.intersection;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Intersection for the game. Can be thought of as the current level.
+ * @author Justin
+ *
+ */
+
+public class Intersection {
+	
+	private int intersectionX;
+	private int intersectionY;
+	private SpriteBatch intersectionBatch;
+	private Texture intersectionImg;
+	private int[] stops;
+	private int[] carSpawns;
+	private int nWays;	//Number of ways for the intersection (3 way, 4way, etc.)
+	private int nLanes;	//Number of lanes per way
+	private String intersectionName;
+
+	/**
+	 * Creates a standard intersection
+	 * @param x
+	 * @param y
+	 * @param ways
+	 * @param lanes
+	 * @param name
+	 * @param batch
+	 * @param img
+	 */
+	public Intersection (int x, int y, int ways, int lanes, String name, SpriteBatch batch, String img){
+		intersectionX = x;
+		intersectionY = y;
+		nWays = ways;
+		nLanes = lanes;
+		nWays = ways;
+		intersectionName = name;
+		intersectionBatch = batch;
+		intersectionImg = new Texture(img);
+	}
+	
+	/**
+	 * Loads an intersection from a file.
+	 * @param name
+	 */
+	public Intersection(String name){
+		//Work in progress
+	}
+	
+	public void draw(){
+		intersectionBatch.draw(intersectionImg, intersectionX, intersectionY);
+		return;
+	}
+}
